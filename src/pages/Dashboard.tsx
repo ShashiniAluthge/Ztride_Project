96016946edc023a53f37f59220b9e2ba1fcfbd08
Shashini <ssshashini21@gmail.com>
2025-03-21 20:14:52 +0530

    create top bar

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -7,11 +7,11 @@ const Dashboard = () => {
       <div className="w-[235px] bg-blue-500 h-full fixed top-0 left-0">
         <SideNavBar />
       </div>
-      <div className="flex flex-col flex-1 ml-[237px]">
-        <div className="w-full h-[101px] bg-red-500">
+      <div className="flex flex-col flex-1 ml-[269px] mr-[32px]">
+        <div className="w-full h-[101px] bg-[var(--background)]">
           <TopNavBar />
         </div>
-        <div className="flex-1 bg-gray text-[var(--primary)]">
+        <div className="flex-1 bg-black text-[var(--primary)]">
           <h1>content</h1>
         </div>
       </div>
