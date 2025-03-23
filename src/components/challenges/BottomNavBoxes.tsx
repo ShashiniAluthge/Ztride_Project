52cdbc14a70d2d9edf9c6e13426c0402055da1d1
Shashini <ssshashini21@gmail.com>
2025-03-23 22:55:28 +0530

    add bottomNavBoxes component

--- src/components/challenges/BottomNavBoxes.tsx
+++ src/components/challenges/BottomNavBoxes.tsx
@@ -1,8 +1,17 @@
-
+import rightArrow from "../../assets/rightArrow.png";
 
 const BottomNavBoxes = () => {
   return (
-    <div>BottomNavBoxes</div>
+    <div className="my-[21px]">
+    <div className="flex flex-row gap-[4px] ">
+      <div className="bg-[var(--secondary)] w-[25px] h-[25px] border-[0.6px] border-[var(--secondary)] flex items-center justify-center text-[var(--background)]" >1</div>
+      <div className="bg-[var(--background)] w-[25px] h-[25px] border-[0.6px] border-[var(--secondary)] flex items-center justify-center text-[var(--secondary)]">2</div>
+      <div className="bg-[var(--background)] w-[25px] h-[25px] border-[0.6px] border-[var(--secondary)] flex items-center justify-center text-[var(--secondary)]">3</div>
+      <div className="bg-[var(--background)] w-[25px] h-[25px] border-[0.6px] border-[var(--secondary)] flex items-center justify-center text-[var(--secondary)]">
+        <img src={rightArrow}/>
+      </div>
+    </div>
+  </div>
   )
 }
 
